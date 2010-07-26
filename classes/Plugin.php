45983bbc3bb72386bbb35106b6ffcf6b33cdbdce
noother <noothy@gmail.com>
2010-07-26 20:55:21 +0200

    fixed a concurreny security breach with isAuthorized() where attackers could inject code into, for example, !raw

--- classes/Plugin.php
+++ classes/Plugin.php
@@ -1,24 +1,5 @@
 <?php
 
-/*
-	This file is part of Nimda - An advanced event-driven IRC Bot written in PHP with a nice plugin system
-	Copyright (C) 2009  noother [noothy@gmail.com]
-
-	This program is free software; you can redistribute it and/or
-	modify it under the terms of the GNU General Public License
-	as published by the Free Software Foundation; either version 2
-	of the License, or (at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
-*/
-
 class Plugin {
 
 	public  $name        = "";
@@ -115,9 +96,12 @@ class Plugin {
 	
 	function trigger() {
 		$this->output = array();
+			$info_save = $this->info;
 		if($this->IRCBot->isAuthorized($this->info['nick'],$this->level)) {
+			$this->info = $info_save;
 			$this->isTriggered();
 		} else {
+			$this->info = $info_save;
 			$this->IRCBot->sendPrivmsg($this->info['target'],sprintf($this->IRCBot->CONFIG['text_not_authorized'],$this->level,$this->IRCBot->lastLevel));
 		}
 	}
